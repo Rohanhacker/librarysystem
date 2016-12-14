4ce95e8f80ca9e26f232e59fd5b5f1ec5f1c2f89
Rohan <rohan@localhost.localdomain>
2016-12-14 22:43:51 +0530

    form complete but need to update cors at grails api

--- js/createBook.js
+++ js/createBook.js
@@ -1,14 +1,81 @@
 import React from 'react'
-import {Grid, Row, Col, PageHeader} from 'react-bootstrap'
+import {Grid, Row, Col, Button, PageHeader, Form, FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap'
 import Navbar from './Navbar'
 import axios from 'axios'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 class createBook extends React.Component {
   constructor(props) {
     super(props)
+    this.validateIsbn = this.validateIsbn.bind(this)
+    this.handleTitleChange = this.handleTitleChange.bind(this)
+    this.handleAuthorChange = this.handleAuthorChange.bind(this)
+    this.handlePublisherChange = this.handlePublisherChange.bind(this)
+    this.handleIsbnChange = this.handleIsbnChange.bind(this)
+    this.handleEditionChange = this.handleEditionChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
     this.state = {
-      submitted: false
+      submitted: false,
+      title: '',
+      author: '',
+      publisher: '',
+      'isbn': '',
+      'edition': ''
     }
+  }
+  validateIsbn() {
+    const length = this.state.isbn.length
+    if (length === 13) return 'success'
+    else return 'error'
+  }
+  handleTitleChange(event) {
+    this.setState({
+      title: event.target.value
+    })
+  }
+  handleAuthorChange(event) {
+    this.setState({
+      author: event.target.value
+    })
+  }
+  handlePublisherChange(event) {
+    this.setState({
+      publisher: event.target.value
+    })
+  }
+  handleEditionChange(event) {
+    this.setState({
+      edition: event.target.value
+    })
+  }
+  handleIsbnChange(event) {
+    this.setState({
+      isbn: event.target.value
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({
+      submitted: true
+    })
+    axios.post('http://localhost:9123/Book', {
+    title: this.state.title,
+    edition: this.state.edition,
+    author: [{
+      name: this.state.author
+    }],
+    isbn: this.state.isbn,
+    publisher: {
+      name: this.state.publisher
+    }
+  })
+  .then(function (response) {
+    console.log(response)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
   }
   render() {
     return (
@@ -18,7 +85,32 @@ class createBook extends React.Component {
           <Grid>
             <Row className='show-grid'>
               <Col xs={12} md={8} mdOffset={2}>
-                Form here
+                <Form onSubmit={this.handleSubmit}>
+                  <FormGroup>
+                    <ControlLabel>Book Title</ControlLabel>
+                    <FormControl onChange={this.handleTitleChange} type='text' value={this.state.title} placeholder='Book Title' />
+                  </FormGroup>
+                  <FormGroup validationState={this.validateIsbn()}>
+                    <ControlLabel>ISBN</ControlLabel>
+                    <FormControl onChange={this.handleIsbnChange} type='text' value={this.state.isbn} placeholder='ISBN' />
+                    <HelpBlock>{this.validateIsbn}</HelpBlock>
+                  </FormGroup>
+                  <FormGroup>
+                    <ControlLabel>Edition </ControlLabel>
+                    <FormControl onChange={this.handleEditionChange} type='number' value={this.state.edition} placeholder='Edition' />
+                  </FormGroup>
+                  <FormGroup>
+                    <ControlLabel>Publisher's Name</ControlLabel>
+                    <FormControl onChange={this.handlePublisherChange} type='text' value={this.state.publisher} placeholder='Publishers Name' />
+                  </FormGroup>
+                  <FormGroup>
+                    <ControlLabel>Authors Name </ControlLabel>
+                    <FormControl onChange={this.handleAuthorChange} type='text' value={this.state.author} placeholder='Authors Name' />
+                  </FormGroup>
+                  <FormGroup>
+                    <input type='submit' />
+                  </FormGroup>
+                </Form>
               </Col>
             </Row>
           </Grid>
